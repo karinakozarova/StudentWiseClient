703274e7e64c3a7160b191460807244ee3cccea3
Evgeniy Terziev <i425986@fhict.nl>
2019-12-14 13:57:17 +0100

    Changed font to Oswald

--- StudentWiseClient/Register.Designer.cs
+++ StudentWiseClient/Register.Designer.cs
@@ -47,11 +47,11 @@
             // CreateAccountLbl
             // 
             this.CreateAccountLbl.AutoSize = true;
-            this.CreateAccountLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CreateAccountLbl.Location = new System.Drawing.Point(157, 37);
+            this.CreateAccountLbl.Font = new System.Drawing.Font("Oswald", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateAccountLbl.Location = new System.Drawing.Point(160, 22);
             this.CreateAccountLbl.MaximumSize = new System.Drawing.Size(250, 0);
             this.CreateAccountLbl.Name = "CreateAccountLbl";
-            this.CreateAccountLbl.Size = new System.Drawing.Size(249, 110);
+            this.CreateAccountLbl.Size = new System.Drawing.Size(192, 146);
             this.CreateAccountLbl.TabIndex = 3;
             this.CreateAccountLbl.Text = "Create an account";
             // 
@@ -67,84 +67,84 @@
             // 
             // emailAddressTbx
             // 
-            this.emailAddressTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailAddressTbx.Location = new System.Drawing.Point(24, 329);
+            this.emailAddressTbx.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailAddressTbx.Location = new System.Drawing.Point(24, 321);
             this.emailAddressTbx.Name = "emailAddressTbx";
-            this.emailAddressTbx.Size = new System.Drawing.Size(364, 38);
+            this.emailAddressTbx.Size = new System.Drawing.Size(364, 37);
             this.emailAddressTbx.TabIndex = 4;
             // 
             // passwordTbx
             // 
-            this.passwordTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordTbx.Location = new System.Drawing.Point(24, 422);
+            this.passwordTbx.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTbx.Location = new System.Drawing.Point(24, 416);
             this.passwordTbx.Name = "passwordTbx";
             this.passwordTbx.PasswordChar = '*';
-            this.passwordTbx.Size = new System.Drawing.Size(364, 38);
+            this.passwordTbx.Size = new System.Drawing.Size(364, 37);
             this.passwordTbx.TabIndex = 5;
             // 
             // fNameTbx
             // 
-            this.fNameTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fNameTbx.Location = new System.Drawing.Point(24, 238);
+            this.fNameTbx.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fNameTbx.Location = new System.Drawing.Point(24, 226);
             this.fNameTbx.Name = "fNameTbx";
-            this.fNameTbx.Size = new System.Drawing.Size(171, 38);
+            this.fNameTbx.Size = new System.Drawing.Size(171, 37);
             this.fNameTbx.TabIndex = 7;
             // 
             // lNameTbx
             // 
-            this.lNameTbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lNameTbx.Location = new System.Drawing.Point(219, 238);
+            this.lNameTbx.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNameTbx.Location = new System.Drawing.Point(219, 226);
             this.lNameTbx.Name = "lNameTbx";
-            this.lNameTbx.Size = new System.Drawing.Size(171, 38);
+            this.lNameTbx.Size = new System.Drawing.Size(171, 37);
             this.lNameTbx.TabIndex = 8;
             // 
             // emailAddressLbl
             // 
             this.emailAddressLbl.AutoSize = true;
-            this.emailAddressLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailAddressLbl.Location = new System.Drawing.Point(18, 290);
+            this.emailAddressLbl.Font = new System.Drawing.Font("Oswald", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailAddressLbl.Location = new System.Drawing.Point(17, 267);
             this.emailAddressLbl.Name = "emailAddressLbl";
-            this.emailAddressLbl.Size = new System.Drawing.Size(210, 36);
+            this.emailAddressLbl.Size = new System.Drawing.Size(178, 51);
             this.emailAddressLbl.TabIndex = 9;
             this.emailAddressLbl.Text = "Email address:";
             // 
             // passwordLbl
             // 
             this.passwordLbl.AutoSize = true;
-            this.passwordLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordLbl.Location = new System.Drawing.Point(18, 383);
+            this.passwordLbl.Font = new System.Drawing.Font("Oswald", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLbl.Location = new System.Drawing.Point(15, 362);
             this.passwordLbl.Name = "passwordLbl";
-            this.passwordLbl.Size = new System.Drawing.Size(155, 36);
+            this.passwordLbl.Size = new System.Drawing.Size(131, 51);
             this.passwordLbl.TabIndex = 10;
             this.passwordLbl.Text = "Password:";
             // 
             // fNameLbl
             // 
             this.fNameLbl.AutoSize = true;
-            this.fNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fNameLbl.Location = new System.Drawing.Point(18, 190);
+            this.fNameLbl.Font = new System.Drawing.Font("Oswald", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fNameLbl.Location = new System.Drawing.Point(15, 172);
             this.fNameLbl.Name = "fNameLbl";
-            this.fNameLbl.Size = new System.Drawing.Size(161, 36);
+            this.fNameLbl.Size = new System.Drawing.Size(143, 51);
             this.fNameLbl.TabIndex = 11;
             this.fNameLbl.Text = "First name:";
             // 
             // lNameLbl
             // 
             this.lNameLbl.AutoSize = true;
-            this.lNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lNameLbl.Location = new System.Drawing.Point(213, 190);
+            this.lNameLbl.Font = new System.Drawing.Font("Oswald", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNameLbl.Location = new System.Drawing.Point(210, 172);
             this.lNameLbl.Name = "lNameLbl";
-            this.lNameLbl.Size = new System.Drawing.Size(159, 36);
+            this.lNameLbl.Size = new System.Drawing.Size(139, 51);
             this.lNameLbl.TabIndex = 12;
             this.lNameLbl.Text = "Last name:";
             // 
             // loginLinkL
             // 
             this.loginLinkL.AutoSize = true;
-            this.loginLinkL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginLinkL.Location = new System.Drawing.Point(168, 565);
+            this.loginLinkL.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLinkL.Location = new System.Drawing.Point(167, 566);
             this.loginLinkL.Name = "loginLinkL";
-            this.loginLinkL.Size = new System.Drawing.Size(66, 25);
+            this.loginLinkL.Size = new System.Drawing.Size(59, 35);
             this.loginLinkL.TabIndex = 15;
             this.loginLinkL.TabStop = true;
             this.loginLinkL.Text = "Login!";
@@ -153,18 +153,18 @@
             // textLbl
             // 
             this.textLbl.AutoSize = true;
-            this.textLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textLbl.Location = new System.Drawing.Point(83, 540);
+            this.textLbl.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLbl.Location = new System.Drawing.Point(95, 531);
             this.textLbl.Name = "textLbl";
-            this.textLbl.Size = new System.Drawing.Size(239, 25);
+            this.textLbl.Size = new System.Drawing.Size(205, 35);
             this.textLbl.TabIndex = 14;
             this.textLbl.Text = "Already have an account?";
             // 
             // registerBtn
             // 
             this.registerBtn.BackColor = System.Drawing.Color.LawnGreen;
-            this.registerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.registerBtn.Location = new System.Drawing.Point(74, 476);
+            this.registerBtn.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerBtn.Location = new System.Drawing.Point(73, 467);
             this.registerBtn.Name = "registerBtn";
             this.registerBtn.Size = new System.Drawing.Size(255, 61);
             this.registerBtn.TabIndex = 13;
@@ -176,7 +176,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 599);
+            this.ClientSize = new System.Drawing.Size(417, 610);
             this.Controls.Add(this.loginLinkL);
             this.Controls.Add(this.textLbl);
             this.Controls.Add(this.registerBtn);

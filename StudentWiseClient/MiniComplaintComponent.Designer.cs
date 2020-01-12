ce09f6574be15dd6181d52dde87ede70a5c95a05
karinakozarova <karinakozarova1303@gmail.com>
2020-01-12 21:11:16 +0100

    added mini complaint component

---
+++ StudentWiseClient/MiniComplaintComponent.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace StudentWiseClient
+{
+    partial class MiniComplaintComponent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLbl.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.titleLbl.Location = new System.Drawing.Point(20, 40);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(174, 35);
+            this.titleLbl.TabIndex = 3;
+            this.titleLbl.Text = "Title of the complaint";
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Font = new System.Drawing.Font("Oswald", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLbl.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.statusLbl.Location = new System.Drawing.Point(22, 16);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(79, 24);
+            this.statusLbl.TabIndex = 2;
+            this.statusLbl.Text = "IN PROGRESS";
+            // 
+            // MiniComplaintComponent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.titleLbl);
+            this.Controls.Add(this.statusLbl);
+            this.Name = "MiniComplaintComponent";
+            this.Size = new System.Drawing.Size(384, 91);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Label statusLbl;
+    }
+}
---
+++ StudentWiseClient/MiniComplaintComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StudentWiseApi;
+
+namespace StudentWiseClient
+{
+    public partial class MiniComplaintComponent : UserControl
+    {
+        public MiniComplaintComponent()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeComplainStatus(ComplaintStatus status)
+        {
+            statusLbl.Text = status.ToString();
+        }
+
+        public void ChangeTitle(String title)
+        {
+            titleLbl.Text = title;
+        }
+
+
+        public void ChangeLabels(String title, ComplaintStatus status)
+        {
+            ChangeTitle(title);
+            ChangeComplainStatus(status);
+        }
+    }
+}

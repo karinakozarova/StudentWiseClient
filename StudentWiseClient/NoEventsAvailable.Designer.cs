6040d398a6f3a71a885d4d204a519948995ad6ee
karinakozarova <karinakozarova1303@gmail.com>
2020-01-16 00:40:24 +0100

    added no event available user control

---
+++ StudentWiseClient/NoEventsAvailable.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace StudentWiseClient
+{
+    partial class NoEventsAvailable
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sorryLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sorryLbl
+            // 
+            this.sorryLbl.AutoSize = true;
+            this.sorryLbl.Font = new System.Drawing.Font("Oswald", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sorryLbl.Location = new System.Drawing.Point(485, 57);
+            this.sorryLbl.Name = "sorryLbl";
+            this.sorryLbl.Size = new System.Drawing.Size(171, 102);
+            this.sorryLbl.TabIndex = 0;
+            this.sorryLbl.Text = "Sorry!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(303, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(670, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You don\'t have an event today. Maybe you want to add one?";
+            // 
+            // NoEventsAvailable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sorryLbl);
+            this.Name = "NoEventsAvailable";
+            this.Size = new System.Drawing.Size(1098, 255);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sorryLbl;
+        private System.Windows.Forms.Label label1;
+    }
+}
